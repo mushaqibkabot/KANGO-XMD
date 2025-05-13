ac26612e905ad862b7dfffb67126436e16f32880
mushaqibkabot <mushaqibkabot@gmail.com>
2025-05-13 23:44:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~zVNiiALK#T1Tak532krMLJSCEcq2ZY1jVcxkaihVWNj17FYbTXoE' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -25,19 +25,19 @@ global.botname = process.env.BOT_NAME || '𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '252905309953' 
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || '⟹P͙R͙A͜͡  𝗜M͙E⥃M̳𝗔K̆̈卄i͎R͜͡ 𝗂' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿" 
+global.packname = process.env.STICKER_PACK_NAME || "⟹P͙R͙A͜͡  𝗜M͙E⥃M̳𝗔K̆̈卄i͎R͜͡ 𝗂" 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
-global.author = process.env.STICKER_AUTHOR_NAME || "Hector" 
+global.author = process.env.STICKER_AUTHOR_NAME || "⟹P͙R͙A͜͡  𝗜M͙E⥃M̳𝗔K̆̈卄i͎R͜͡ 𝗂" 
 
 //----------------------[ TIMEZONE ]--------------------//
 
